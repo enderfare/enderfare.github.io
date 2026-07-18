16ab3d7ac48dc32c29dd8a5b8e7167d2a190b00b
BetaPoisoner <sdhds954@gmail.com>
2026-07-18 02:07:46 +0200

    Really fix rs script

--- scripts/generate-rss.js
+++ scripts/generate-rss.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 const postsPath = path.join(__dirname, '../data/blog.json');
-const posts = JSON.parse(fs.readFileSync(postsPath, 'utf8'));
+const blog = JSON.parse(fs.readFileSync(postsPath, 'utf8'));
 
 const siteUrl = 'https://enderfare.github.io';
 const now = new Date().toUTCString();
 
 let items = '';
-posts.forEach(post => {
-    items += `
+blog.posts.forEach(post => {
+  items += `
     <item>
       <title>${escapeXml(post.title)}</title>
       <link>${siteUrl}/blog/post.html?post=${post.slug || post.id}</link>
@@ -36,14 +36,14 @@ fs.writeFileSync(path.join(__dirname, '../feed.xml'), feed);
 console.log('RSS feed generated!');
 
 function escapeXml(unsafe) {
-    if (!unsafe) return '';
-    return unsafe.replace(/[<>&'"]/g, function (c) {
-        switch (c) {
-            case '<': return '&lt;';
-            case '>': return '&gt;';
-            case '&': return '&amp;';
-            case '"': return '&quot;';
-            case "'": return '&apos;';
-        }
-    });
+  if (!unsafe) return '';
+  return unsafe.replace(/[<>&'"]/g, function (c) {
+    switch (c) {
+      case '<': return '&lt;';
+      case '>': return '&gt;';
+      case '&': return '&amp;';
+      case '"': return '&quot;';
+      case "'": return '&apos;';
+    }
+  });
 }
